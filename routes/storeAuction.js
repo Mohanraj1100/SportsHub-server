b76f32ddbf43ac9f74e6e77a987c8f19b4e12510
Mohanraj1100 <mohanraj.anbuselvam@tringapps.com>
2023-08-07 18:43:27 +0530

    initial

--- routes/storeAuction.js
+++ routes/storeAuction.js
@@ -1,5 +1,42 @@
+const express = require('express')
+const multer = require("multer");
+const pool = require('../db');
+const router = express.Router();
 
+//img store config
+const imgconfig = multer.diskStorage(
+    {
+        destination:(req,file,callback)=>{
+            callback(null,"./uploads")
+        },
+        filename:(req,file,callback)=>{
+            callback(null,`image-${Date.now()}.${file.originalname}`)
+        }
+    }
+)
 
+const upload = multer({
+    storage:imgconfig
+})
 
+router.post('/auctionregister', upload.single("photo"), async (req, res) => {
+    const { type, auctionname, auctiondate, pointsperteam, minimumbid, bidincreaseby, playerperteam, username } = req.body;
+    const { filename } = req.file;
+  
+    try {
+      const idRes = await pool.query("SELECT id FROM users WHERE username = $1", [username]);
+      const id = idRes.rows[0].id; 
+  
+      await pool.query("INSERT INTO createauctions(auctiontype, auctionname, auctiondate, pointsperteam, minbid, bidincrease, playerperteam, profileid,file_name) VALUES ($1, $2, $3, $4, $5, $6, $7, $8,$9)", [type, auctionname, auctiondate, pointsperteam, minimumbid, bidincreaseby, playerperteam, id,filename]);
+  
+      console.log("Data added");
+      res.status(201).json({ message: "File uploaded successfully!" });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ message: "Error uploading data" });
+    }
+  });
+
+  module.exports = router
 
 
