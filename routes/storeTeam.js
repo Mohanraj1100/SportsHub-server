5f73e199c5551a9b1a19571a75263cb1e358330a
Mohanraj1100 <mohanraj.anbuselvam@tringapps.com>
2023-08-08 18:47:04 +0530

    user2 update

--- routes/storeTeam.js
+++ routes/storeTeam.js
@@ -22,16 +22,15 @@ const upload = multer({
 router.post('/teamregister', upload.single("photo"), async (req, res) => {
     const { teamname, teamshortname, shortcutkey, auctionid } = req.body;
     const { filename } = req.file;
-    console.log(req);
+    // console.log(req);
 
     try {
           const idRes = await pool.query("SELECT auctionname FROM createauctions WHERE id = $1", [auctionid]);
-          console.log(idRes);
-        //   const id = idRes.rows[0]; 
-
-        // await pool.query("INSERT INTO team_details(teamname, teamshortname, shortcutkey, auctionname,file_name) VALUES ($1, $2, $3, $4, $5, $6, $7, $8,$9)", [type, auctionname, auctiondate, pointsperteam, minimumbid, bidincreaseby, playerperteam, id, filename]);
+        //   console.log(idRes);
+          const auctionname = idRes.rows[0].auctionname; 
+        await pool.query("INSERT INTO team_details(teamname, teamshortname, teamshortcutkey, auctionname,teamfilename) VALUES ($1, $2, $3, $4, $5)", [ teamname, teamshortname, shortcutkey,auctionname,filename]);
         console.log("Data added");
-        // res.status(201).json({ message: "File uploaded successfully!" });
+        res.status(201).json({ message: "File uploaded successfully!" });
     } catch (error) {
         console.log(error.message);
         res.status(500).json({ message: "Error uploading data" });
@@ -42,12 +41,16 @@ router.post('/teamregister', upload.single("photo"), async (req, res) => {
 router.get('/teams', async (req, res) => {
     try {
         const { id } = req.query;
+        // console.log(id);
         const auctionname = await pool.query('select auctionname from createauctions where id = $1', [id]);
-        if (auctionname.rows.length == 0) {
-            return res.status(403).json({ error: 'no auction found' });
-        }
+        const auctionoriginal = auctionname.rows[0].auctionname;
+        console.log(auctionoriginal);
+        // if (auctionname.rows.length == 0) {
+        //     return res.status(403).json({ error: 'no auction found' });
+        // }
 
-        const teamDetails = await pool.query('select team_id,teamname,teamshortname,teamshortcutkey from team_details where auctionname = $1,'[auctionname])
+        const teamDetails = await pool.query('select team_id,teamname,teamshortname,teamshortcutkey from team_details where auctionname = $1',[auctionoriginal])
+        console.log(teamDetails);
         res.json(teamDetails.rows)
     }
     catch (error) {
@@ -55,6 +58,23 @@ router.get('/teams', async (req, res) => {
     }
 
 })
+
+router.put('/updateteam',async(req,res)=>{
+
+    const {teamname,teamshortname,shortcutkey,teamid} = req.body;
+    try{
+    const updatedQuery = await pool.query('UPDATE team_details set teamname = $1, teamshortname = $2, teamshortcutkey = $3 WHERE team_id = $4',[teamname,teamshortname,shortcutkey,teamid])
+    console.log("updated Sucessfully");
+    return res.status(200).json({ message: "updated Sucessfully" });
+
+    }
+    catch(error)
+    {
+        console.log(error.message);
+    }
+})
+
+
 module.exports = router
 
 
