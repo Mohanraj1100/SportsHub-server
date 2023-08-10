f08bc5e2e69f7b87720cb5e17574bd8313faf8ff
NITHISH0403 <nithishkumar121212@gmail.com>
2023-08-10 10:26:53 +0530

    user1 update

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const registerLogin=require('./routes/registerLogin');
 const Dashboard = require('./routes/Dashboard');
 const storeAuction = require('./routes/storeAuction')
 const storeTeam = require('./routes/storeTeam')
+const OpenPanal = require('./routes/OpenPanal')
 app.use(cors());
 app.use(express.json()) 
 app.use("/uploads",express.static("./uploads"))
@@ -14,6 +15,7 @@ app.use(registerLogin);
 app.use(Dashboard);
 app.use(storeAuction);
 app.use(storeTeam);
+app.use(OpenPanal);
 
 app.use((req,res,next)=>{
     res.status(404).send("Page not found");
