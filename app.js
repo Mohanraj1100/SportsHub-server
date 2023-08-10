f9d1b49b43d97e675820d8acc4121417f89413ca
Mohanraj1100 <mohanraj.anbuselvam@tringapps.com>
2023-08-10 10:49:10 +0530

    mainupdate

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const registerLogin=require('./Routes/registerLogin');
 const Dashboard = require('./Routes/Dashboard');
 const storeAuction = require('./Routes/storeAuction')
 const storeTeam = require('./routes/storeTeam')
+const storePlayer = require('./routes/storePlayer')
+
 app.use(cors());
 app.use(express.json()) 
 app.use("/uploads",express.static("./uploads"))
@@ -14,6 +16,8 @@ app.use(registerLogin);
 app.use(Dashboard);
 app.use(storeAuction);
 app.use(storeTeam);
+app.use(storePlayer);
+
 
 app.use((req,res,next)=>{
     res.status(404).send("Page not found");
