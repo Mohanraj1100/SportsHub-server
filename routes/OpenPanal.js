c1e50a3da827fb6427f2e31025af67c48e6dae5e
NITHISH0403 <nithishkumar121212@gmail.com>
2023-08-11 07:42:30 +0530

    user1 update

--- routes/OpenPanal.js
+++ routes/OpenPanal.js
@@ -28,14 +28,14 @@ router.get('/auction', async (req, res) => {
 
     const auctionData = resultAuction.rows[0];
 
-    const resultPlayer = await pool.query('SELECT player_id, playername, playerfilename, teamid, playerstatus FROM player_details WHERE auctionid = $1', [auctionData.id]);
+    const resultPlayer = await pool.query('SELECT player_id, playername, playerfilename, teamid, batting, bowling, wicketkeeper, playerstatus FROM player_details WHERE auctionid = $1', [auctionData.id]);
 
     const players = resultPlayer.rows;
     if (players.length === 0) {
       return res.status(404).json({ error: 'Players not found' });
     }
 
-    const resultTeam = await pool.query('SELECT team_id, teamname, teamfilename FROM team_details WHERE auctionname = $1', [auctionData.auctionname]);
+    const resultTeam = await pool.query('SELECT team_id, teamname, teamfilename, playercount FROM team_details WHERE auctionname = $1', [auctionData.auctionname]);
 
     const teams = resultTeam.rows;
     if (teams.length === 0) {
@@ -51,11 +51,42 @@ router.get('/auction', async (req, res) => {
 
 router.put('/updatesoldplayer',async(req,res)=>{
 
-  const {playerid, teamid, playerstatus } = req.body;
+  const {playerid, teamid, playerStatus, count} = req.body;
   console.log(req.body);
   try{
-  const updatedQuery = await pool.query('UPDATE player_details set teamid = $1, playerstatus = $2 WHERE player_id = $3',[teamid,playerstatus,playerid])
-  console.log("updated Sucessfully");
+  const updatedPlayer = await pool.query('UPDATE player_details set teamid = $1, playerstatus = $2 WHERE player_id = $3',[teamid,playerStatus,playerid]);
+  const updatedTeam = await pool.query('UPDATE team_details set playercount = playercount + $1 WHERE team_id = $2',[count,teamid]);
+  // console.log("updated Sucessfully",updatedQuery);
+  return res.status(200).json({ message: "updated Sucessfully" });
+  }
+  catch(error)
+  {
+      console.log(error.message);
+  }
+})
+
+router.put('/updateunsoldplayer',async(req,res)=>{
+
+  const {playerid, playerStatus } = req.body;
+  console.log(req.body);
+  try{
+  const updatedQuery = await pool.query('UPDATE player_details set playerstatus = $1 WHERE player_id = $2',[playerStatus,playerid])
+  console.log("updated Sucessfully",updatedQuery);
+  return res.status(200).json({ message: "updated Sucessfully" });
+  }
+  catch(error)
+  {
+      console.log(error.message);
+  }
+})
+
+router.put('/updateunplayercount',async(req,res)=>{
+
+  const {teamid, } = req.body;
+  console.log(req.body);
+  try{
+  const updatedQuery = await pool.query('UPDATE team_details set playercount = playercount+$1 WHERE team_id = $2',[count,teamid])
+  console.log("updated Sucessfully",updatedQuery);
   return res.status(200).json({ message: "updated Sucessfully" });
   }
   catch(error)
